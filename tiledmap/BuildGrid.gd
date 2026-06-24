994eeea2cd93059b34150af7e84fd264a242132e
Phạm Hoàng Huân <hoanghuanpham3@gmail.com>
2026-06-24 22:47:58 +0700

    feat: improve mouse tracking placement

--- tiledmap/BuildGrid.gd
+++ tiledmap/BuildGrid.gd
@@ -14,8 +14,9 @@ extends GridMap
 @export var alternate_ground_item := 7
 @export var noise_ground_item := 8
 @export var block_item := 5
-@export var allow_side_face_placement := false
-@export var max_raycast_steps := 512
+@export var allow_side_face_placement := true
+@export var require_ground_under_blocks := false
+@export var require_block_support := false
 @export var raycast_epsilon := 0.0001
 
 @export var valid_preview_color := Color(0.15, 0.85, 1.0, 0.35)
@@ -148,8 +149,8 @@ func _get_mouse_target(mouse_position: Vector2 = Vector2(-1.0, -1.0)) -> Diction
 	var block_hit := _raycast_blocks(ray_origin, ray_direction, max_distance)
 
 	if not block_hit.is_empty():
-		var hit_cell: Vector3i = block_hit["cell"]
-		var hit_normal: Vector3i = block_hit["normal"]
+		var hit_cell := Vector3i(block_hit["cell"])
+		var hit_normal := Vector3i(block_hit["normal"])
 		var is_top_face := hit_normal == Vector3i.UP
 
 		var place_cell := hit_cell + hit_normal
@@ -178,54 +179,32 @@ func _raycast_blocks(ray_origin: Vector3, ray_direction: Vector3, max_distance: 
 	if ray_direction == Vector3.ZERO:
 		return {}
 
-	var ray_start := ray_origin + ray_direction * raycast_epsilon
-	var cell := _point_to_build_cell(ray_start)
-	var distance := 0.0
-	var step := Vector3i(
-		_sign_nonzero(ray_direction.x),
-		_sign_nonzero(ray_direction.y),
-		_sign_nonzero(ray_direction.z)
-	)
+	var best_cell := Vector3i.ZERO
+	var best_normal := Vector3i.ZERO
+	var best_distance := max_distance + raycast_epsilon
+	var has_hit := false
 
-	var next_x := _next_x_boundary(cell.x, step.x)
-	var next_y := _next_y_boundary(cell.y, step.y)
-	var next_z := _next_z_boundary(cell.z, step.z)
+	for cell in get_used_cells():
+		if not _is_build_block_cell(cell):
+			continue
 
-	var t_max_x := _axis_distance(ray_start.x, ray_direction.x, next_x)
-	var t_max_y := _axis_distance(ray_start.y, ray_direction.y, next_y)
-	var t_max_z := _axis_distance(ray_start.z, ray_direction.z, next_z)
-	var t_delta_x := _axis_step_distance(ray_direction.x, cell_size.x)
-	var t_delta_y := _axis_step_distance(ray_direction.y, cell_size.y)
-	var t_delta_z := _axis_step_distance(ray_direction.z, cell_size.z)
+		var hit := _ray_intersects_cell_aabb(ray_origin, ray_direction, _get_block_aabb(cell))
+		if hit.is_empty():
+			continue
 
-	for _i in range(max_raycast_steps):
-		if distance > max_distance:
-			break
+		var hit_distance := float(hit["distance"])
+		if hit_distance < best_distance:
+			best_distance = hit_distance
+			best_cell = cell
+			best_normal = Vector3i(hit["normal"])
+			has_hit = true
 
-		if _is_build_block_cell(cell):
-			var aabb := _get_block_aabb(cell)
-			var hit = aabb.intersects_ray(ray_origin, ray_direction)
-			if hit != null:
-				var hit_distance := ray_origin.distance_to(hit)
-				if hit_distance <= max_distance + raycast_epsilon:
-					return {
-						"cell": cell,
-						"normal": _get_aabb_hit_normal(aabb, hit),
-						"distance_sq": ray_origin.distance_squared_to(hit),
-					}
-
-		if t_max_x <= t_max_y and t_max_x <= t_max_z:
-			distance = t_max_x
-			t_max_x += t_delta_x
-			cell.x += step.x
-		elif t_max_y <= t_max_z:
-			distance = t_max_y
-			t_max_y += t_delta_y
-			cell.y += step.y
-		else:
-			distance = t_max_z
-			t_max_z += t_delta_z
-			cell.z += step.z
+	if has_hit:
+		return {
+			"cell": best_cell,
+			"normal": best_normal,
+			"distance_sq": best_distance * best_distance,
+		}
 
 	return {}
 
@@ -249,14 +228,6 @@ func _point_to_ground_cell(point: Vector3) -> Vector3i:
 	)
 
 
-func _point_to_build_cell(point: Vector3) -> Vector3i:
-	return Vector3i(
-		floori(point.x / cell_size.x + 0.5),
-		floori(point.y / cell_size.y) + 1,
-		floori(point.z / cell_size.z + 0.5)
-	)
-
-
 func _get_top_block_y(x: int, z: int) -> int:
 	var top_y := 0
 
@@ -268,13 +239,16 @@ func _get_top_block_y(x: int, z: int) -> int:
 
 
 func _can_place_block(cell: Vector3i) -> bool:
-	return (
-		cell.y > 0
-		and cell.y <= max_build_height
-		and _has_ground(cell.x, cell.z)
-		and get_cell_item(cell) == INVALID_CELL_ITEM
-		and _has_support(cell)
-	)
+	if cell.y <= 0 or cell.y > max_build_height:
+		return false
+	if get_cell_item(cell) != INVALID_CELL_ITEM:
+		return false
+	if require_ground_under_blocks and not _has_ground(cell.x, cell.z):
+		return false
+	if require_block_support and not _has_support(cell):
+		return false
+
+	return true
 
 
 func _has_support(cell: Vector3i) -> bool:
@@ -302,58 +276,121 @@ func _get_block_aabb(cell: Vector3i) -> AABB:
 	return AABB(center - half_size, cell_size)
 
 
-func _get_aabb_hit_normal(aabb: AABB, hit_position: Vector3) -> Vector3i:
-	var local_hit := hit_position - aabb.get_center()
-	var extents := aabb.size * 0.5
-	var x_distance := absf(absf(local_hit.x) - extents.x)
-	var y_distance := absf(absf(local_hit.y) - extents.y)
-	var z_distance := absf(absf(local_hit.z) - extents.z)
+func _ray_intersects_cell_aabb(ray_origin: Vector3, ray_direction: Vector3, aabb: AABB) -> Dictionary:
+	var min_point := aabb.position
+	var max_point := aabb.position + aabb.size
+	var enter_distance := -INF
+	var exit_distance := INF
+	var enter_normal := Vector3i.ZERO
+	var exit_normal := Vector3i.ZERO
 
-	if y_distance <= x_distance and y_distance <= z_distance:
-		return Vector3i.UP if local_hit.y >= 0.0 else Vector3i.DOWN
-	if x_distance <= z_distance:
-		return Vector3i.RIGHT if local_hit.x >= 0.0 else Vector3i.LEFT
-	return Vector3i.BACK if local_hit.z >= 0.0 else Vector3i.FORWARD
+	var x_result := _intersect_axis(
+		ray_origin.x,
+		ray_direction.x,
+		min_point.x,
+		max_point.x,
+		Vector3i.LEFT,
+		Vector3i.RIGHT
+	)
+	if x_result.is_empty():
+		return {}
+	enter_distance = float(x_result["enter_distance"])
+	exit_distance = float(x_result["exit_distance"])
+	enter_normal = Vector3i(x_result["enter_normal"])
+	exit_normal = Vector3i(x_result["exit_normal"])
+
+	var y_result := _intersect_axis(
+		ray_origin.y,
+		ray_direction.y,
+		min_point.y,
+		max_point.y,
+		Vector3i.DOWN,
+		Vector3i.UP
+	)
+	if y_result.is_empty():
+		return {}
+	var y_enter_distance := float(y_result["enter_distance"])
+	var y_exit_distance := float(y_result["exit_distance"])
+	if y_enter_distance > enter_distance:
+		enter_distance = y_enter_distance
+		enter_normal = Vector3i(y_result["enter_normal"])
+	if y_exit_distance < exit_distance:
+		exit_distance = y_exit_distance
+		exit_normal = Vector3i(y_result["exit_normal"])
+
+	var z_result := _intersect_axis(
+		ray_origin.z,
+		ray_direction.z,
+		min_point.z,
+		max_point.z,
+		Vector3i.FORWARD,
+		Vector3i.BACK
+	)
+	if z_result.is_empty():
+		return {}
+	var z_enter_distance := float(z_result["enter_distance"])
+	var z_exit_distance := float(z_result["exit_distance"])
+	if z_enter_distance > enter_distance:
+		enter_distance = z_enter_distance
+		enter_normal = Vector3i(z_result["enter_normal"])
+	if z_exit_distance < exit_distance:
+		exit_distance = z_exit_distance
+		exit_normal = Vector3i(z_result["exit_normal"])
+
+	if enter_distance > exit_distance or exit_distance < 0.0:
+		return {}
+
+	if enter_distance >= 0.0:
+		return {
+			"distance": enter_distance,
+			"normal": enter_normal,
+		}
+
+	return {
+		"distance": 0.0,
+		"normal": exit_normal,
+	}
 
 
 func _get_block_visual_center(cell: Vector3i) -> Vector3:
 	return map_to_local(cell) + Vector3(0.0, -cell_size.y * 0.5, 0.0)
 
 
-func _sign_nonzero(value: float) -> int:
-	if value < 0.0:
-		return -1
-	return 1
-
-
-func _axis_distance(origin: float, direction: float, boundary: float) -> float:
+func _intersect_axis(
+	origin: float,
+	direction: float,
+	min_value: float,
+	max_value: float,
+	min_normal: Vector3i,
+	max_normal: Vector3i
+) -> Dictionary:
 	if absf(direction) < 0.000001:
-		return INF
-	return maxf((boundary - origin) / direction, 0.0)
+		if origin < min_value or origin > max_value:
+			return {}
+		return {
+			"enter_distance": -INF,
+			"exit_distance": INF,
+			"enter_normal": Vector3i.ZERO,
+			"exit_normal": Vector3i.ZERO,
+		}
 
+	var min_distance := (min_value - origin) / direction
+	var max_distance := (max_value - origin) / direction
 
-func _axis_step_distance(direction: float, size: float) -> float:
-	if absf(direction) < 0.000001:
-		return INF
-	return absf(size / direction)
+	if min_distance <= max_distance:
+		return {
+			"enter_distance": min_distance,
+			"exit_distance": max_distance,
+			"enter_normal": min_normal,
+			"exit_normal": max_normal,
+		}
 
-
-func _next_x_boundary(cell_x: int, step_x: int) -> float:
-	if step_x > 0:
-		return (float(cell_x) + 0.5) * cell_size.x
-	return (float(cell_x) - 0.5) * cell_size.x
-
-
-func _next_y_boundary(cell_y: int, step_y: int) -> float:
-	if step_y > 0:
-		return float(cell_y) * cell_size.y
-	return float(cell_y - 1) * cell_size.y
-
-
-func _next_z_boundary(cell_z: int, step_z: int) -> float:
-	if step_z > 0:
-		return (float(cell_z) + 0.5) * cell_size.z
-	return (float(cell_z) - 0.5) * cell_size.z
+	return {
+		"enter_distance": max_distance,
+		"exit_distance": min_distance,
+		"enter_normal": max_normal,
+		"exit_normal": min_normal,
+	}
 
 
 func _create_preview():
